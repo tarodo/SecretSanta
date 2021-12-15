79fa0bdd25060e3673c90f7c6cb8e03a978e36d3
Roman Volozhanin <rsvolozhanin@gmail.com>
2021-12-15 17:08:26 +0300

    added db models

--- bot/admin.py
+++ bot/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Game
+from .models import Game, GameUser, Wishlist, Interest, Lottery
 # Register your models here.
 
 admin.site.register(Game)
+admin.site.register(GameUser)
+admin.site.register(Wishlist)
+admin.site.register(Interest)
+admin.site.register(Lottery)
