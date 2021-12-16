67f2a04dc2ee4d496a85f80725587bfe0fbacdf7
Roman Volozhanin <rsvolozhanin@gmail.com>
2021-12-16 07:20:35 +0300

    add meta to models

--- bot/models.py
+++ bot/models.py
@@ -7,18 +7,39 @@ class Game(models.Model):
     name = models.CharField('Название игры', max_length=200)
     cost_limit = models.CharField('Лимит на стоимость', blank=True, max_length=40)
     reg_finish = models.DateTimeField('Дата окончания регистраций')
-    delivery = models.DateTimeField('Дата отправления подарока')
+    delivery = models.DateTimeField('Дата отправления подарка')
     created_at = models.DateTimeField(
         'Когда создана игра',
         default=timezone.now)
+
+    def __str__(self):
+        return f'{self.name} : {self.cost_limit} : {self.reg_finish.strftime("%d.%m.%Y %H:%M")}'
+
+    class Meta:
+        verbose_name = 'Игра'
+        verbose_name_plural = 'Игры'
 
 
 class Wishlist(models.Model):
     name = models.CharField('Название подарка', max_length=100)
 
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Вишлист'
+        verbose_name_plural = 'Вишлисты'
+
 
 class Interest(models.Model):
     name = models.CharField('Зона интереса для подарка', max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Интерес'
+        verbose_name_plural = 'Интересы'
 
 
 class GameUser(models.Model):
@@ -31,7 +52,18 @@ class GameUser(models.Model):
     wishlist = models.ManyToManyField(Wishlist, verbose_name="Вишлист игрока")
     interest = models.ManyToManyField(Interest, verbose_name="Интересы игрока")
 
+    def __str__(self):
+        return f'{self.td_id} : {self.name}'
+
+    class Meta:
+        verbose_name = 'Участник'
+        verbose_name_plural = 'Участники'
+
 
 class Lottery(models.Model):
     who = models.OneToOneField(GameUser, on_delete=models.CASCADE, related_name='who', verbose_name="Кто дарит")
     whom = models.OneToOneField(GameUser, on_delete=models.CASCADE, related_name='whom', verbose_name="Кому дарит")
+
+    class Meta:
+        verbose_name = 'Жеребьевка'
+        verbose_name_plural = 'Жеребьевки'
