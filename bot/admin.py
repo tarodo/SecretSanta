acc4129d68e1b87d0b95a6eb1b34931d318f4974
Roman Volozhanin <rsvolozhanin@gmail.com>
2021-12-16 07:44:47 +0300

    correct admin vision

--- bot/admin.py
+++ bot/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 from .models import Game, GameUser, Wishlist, Interest, Lottery
 # Register your models here.
 
-admin.site.register(Game)
-admin.site.register(GameUser)
+
+class GameAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', )
+    list_display = ('name', 'cost_limit', 'reg_finish', 'delivery', )
+
+
+class GameUserAdmin(admin.ModelAdmin):
+    list_display = ('td_id', 'name', 'phone')
+
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(GameUser, GameUserAdmin)
 admin.site.register(Wishlist)
 admin.site.register(Interest)
 admin.site.register(Lottery)
