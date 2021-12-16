70409d95753425652952cd02a24a02a16221014e
Roman Volozhanin <rsvolozhanin@gmail.com>
2021-12-16 07:21:48 +0300

    styled models

--- bot/models.py
+++ bot/models.py
@@ -4,66 +4,71 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Game(models.Model):
-    name = models.CharField('Название игры', max_length=200)
-    cost_limit = models.CharField('Лимит на стоимость', blank=True, max_length=40)
-    reg_finish = models.DateTimeField('Дата окончания регистраций')
-    delivery = models.DateTimeField('Дата отправления подарка')
-    created_at = models.DateTimeField(
-        'Когда создана игра',
-        default=timezone.now)
+    name = models.CharField("Название игры", max_length=200)
+    cost_limit = models.CharField("Лимит на стоимость", blank=True, max_length=40)
+    reg_finish = models.DateTimeField("Дата окончания регистраций")
+    delivery = models.DateTimeField("Дата отправления подарка")
+    created_at = models.DateTimeField("Когда создана игра", default=timezone.now)
 
     def __str__(self):
         return f'{self.name} : {self.cost_limit} : {self.reg_finish.strftime("%d.%m.%Y %H:%M")}'
 
     class Meta:
-        verbose_name = 'Игра'
-        verbose_name_plural = 'Игры'
+        verbose_name = "Игра"
+        verbose_name_plural = "Игры"
 
 
 class Wishlist(models.Model):
-    name = models.CharField('Название подарка', max_length=100)
+    name = models.CharField("Название подарка", max_length=100)
 
     def __str__(self):
-        return f'{self.name}'
+        return f"{self.name}"
 
     class Meta:
-        verbose_name = 'Вишлист'
-        verbose_name_plural = 'Вишлисты'
+        verbose_name = "Вишлист"
+        verbose_name_plural = "Вишлисты"
 
 
 class Interest(models.Model):
-    name = models.CharField('Зона интереса для подарка', max_length=100)
+    name = models.CharField("Зона интереса для подарка", max_length=100)
 
     def __str__(self):
-        return f'{self.name}'
+        return f"{self.name}"
 
     class Meta:
-        verbose_name = 'Интерес'
-        verbose_name_plural = 'Интересы'
+        verbose_name = "Интерес"
+        verbose_name_plural = "Интересы"
 
 
 class GameUser(models.Model):
-    td_id = models.CharField('ID телеграмма', max_length=20)
-    is_admin = models.BooleanField('Админ?', default=False)
-    name = models.CharField('Имя игрока', max_length=200)
+    td_id = models.CharField("ID телеграмма", max_length=20)
+    is_admin = models.BooleanField("Админ?", default=False)
+    name = models.CharField("Имя игрока", max_length=200)
     phone = PhoneNumberField(blank=True, verbose_name="Номер владельца")
-    letter = models.TextField('Письмо Санте')
+    letter = models.TextField("Письмо Санте")
     game = models.ForeignKey(Game, on_delete=models.CASCADE, verbose_name="Игра")
     wishlist = models.ManyToManyField(Wishlist, verbose_name="Вишлист игрока")
     interest = models.ManyToManyField(Interest, verbose_name="Интересы игрока")
 
     def __str__(self):
-        return f'{self.td_id} : {self.name}'
+        return f"{self.td_id} : {self.name}"
 
     class Meta:
-        verbose_name = 'Участник'
-        verbose_name_plural = 'Участники'
+        verbose_name = "Участник"
+        verbose_name_plural = "Участники"
 
 
 class Lottery(models.Model):
-    who = models.OneToOneField(GameUser, on_delete=models.CASCADE, related_name='who', verbose_name="Кто дарит")
-    whom = models.OneToOneField(GameUser, on_delete=models.CASCADE, related_name='whom', verbose_name="Кому дарит")
+    who = models.OneToOneField(
+        GameUser, on_delete=models.CASCADE, related_name="who", verbose_name="Кто дарит"
+    )
+    whom = models.OneToOneField(
+        GameUser,
+        on_delete=models.CASCADE,
+        related_name="whom",
+        verbose_name="Кому дарит",
+    )
 
     class Meta:
-        verbose_name = 'Жеребьевка'
-        verbose_name_plural = 'Жеребьевки'
+        verbose_name = "Жеребьевка"
+        verbose_name_plural = "Жеребьевки"
