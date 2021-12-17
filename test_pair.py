73ec2906d5e675fa0a296672a0d200e5853739e0
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2021-12-17 13:19:57 +0900

    Изменил структуру входящих данных на список словарей

--- test_pair.py
+++ test_pair.py
@@ -3,13 +3,20 @@ import random
 from collections import deque
 
 
-users = ['user_1', 'user_2', 'user_3', 'user_4', 'user_5', 'user_6',]
+users = [
+{'chatid_user_1': ['phone_user_1', 'username_1']},
+{'chatid_user_2': ['phone_user_2', 'username_2']},
+{'chatid_user_3': ['phone_user_3', 'username_3']},
+{'chatid_user_4': ['phone_user_4', 'username_3']},
+{'chatid_user_5': ['phone_user_5', 'username_3']},
+{'chatid_user_6': ['phone_user_6', 'username_3']},
+]
 
 
 def get_created_pair(users):
     user_pairs=[]
     for user in range(len(users)):
-        user_pair = users[user],'готовит подарок для',users[(user+1)%(len(users))]
+        user_pair = users[user], users[(user+1)%(len(users))]
         user_pairs.append(user_pair)
     return user_pairs
 
@@ -18,7 +25,7 @@ def pairup(users):
     random.shuffle(users)
     partners = deque(users)
     partners.rotate()
-    return dict(zip(users, partners))
+    return list(zip(users, partners))
 
 pprint(get_created_pair(users))
 print()
