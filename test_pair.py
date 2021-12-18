e0035b11a35a5bb8c0ad600e124f35ae4ce27029
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2021-12-18 20:38:10 +0900

    Добавил возможность исключать по заданному списку

--- test_pair.py
+++ test_pair.py
@@ -4,29 +4,37 @@ from collections import deque
 
 
 users = [
-{'chatid_user_1': ['phone_user_1', 'username_1']},
-{'chatid_user_2': ['phone_user_2', 'username_2']},
-{'chatid_user_3': ['phone_user_3', 'username_3']},
-{'chatid_user_4': ['phone_user_4', 'username_3']},
-{'chatid_user_5': ['phone_user_5', 'username_3']},
-{'chatid_user_6': ['phone_user_6', 'username_3']},
+'chatid_user_1',
+'chatid_user_2',
+'chatid_user_3',
+'chatid_user_4',
+'chatid_user_5',
+'chatid_user_6',
 ]
 
+not_users_pair = [
+('chatid_user_2', 'chatid_user_5'),
+('chatid_user_3', 'chatid_user_2'),
+('chatid_user_6', 'chatid_user_3'),
+]
 
-def get_created_pair(users):
-    user_pairs=[]
-    for user in range(len(users)):
-        user_pair = users[user], users[(user+1)%(len(users))]
-        user_pairs.append(user_pair)
-    return user_pairs
+# def get_created_pair(users):
+#     user_pairs=[]
+#     for user in range(len(users)):
+#         user_pair = users[user], users[(user+1)%(len(users))]
+#         user_pairs.append(user_pair)
+#     return user_pairs
 
 
-def pairup(users):
+
+def pairup(users, not_users_pair):
     random.shuffle(users)
     partners = deque(users)
     partners.rotate()
-    return list(zip(users, partners))
+    new = list(zip(users, partners))
+    if any(user in new for user in not_users_pair):
+    	return pairup(users, not_users_pair)
+    else:	
+    	return new
 
-pprint(get_created_pair(users))
-print()
-pprint(pairup(users))
+pprint(pairup(users, not_users_pair))
